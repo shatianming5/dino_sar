3444b3e9ed3a348dd7366d5ef796f55ae4cff70f
shatianming5 <shatianming5@users.noreply.github.com>
2025-12-24 23:58:19 -0500

    cb: fix config scoping for rsar cb0p1

--- configs/_base_/datasets/rsar_dota_le90_cb0p1.py
+++ configs/_base_/datasets/rsar_dota_le90_cb0p1.py
@@ -1,11 +1,49 @@
 # dataset settings (RSAR / DOTA-OBB) with class-balanced oversampling
 
-_base_ = ["./rsar_dota_le90.py"]
+dataset_type = "RSARDOTADataset"
+data_root = "./"
 
-# Oversample rare classes following LVIS repeat-factor strategy.
+angle_version = "le90"
+
+img_norm_cfg = dict(mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True)
+
+train_pipeline = [
+    dict(type="LoadImageFromFile"),
+    dict(type="LoadAnnotations", with_bbox=True),
+    dict(type="RResize", img_scale=(800, 800)),
+    dict(
+        type="RRandomFlip",
+        flip_ratio=[0.25, 0.25, 0.25],
+        direction=["horizontal", "vertical", "diagonal"],
+        version=angle_version,
+    ),
+    dict(type="Normalize", **img_norm_cfg),
+    dict(type="Pad", size_divisor=32),
+    dict(type="DefaultFormatBundle"),
+    dict(type="Collect", keys=["img", "gt_bboxes", "gt_labels"]),
+]
+
+test_pipeline = [
+    dict(type="LoadImageFromFile"),
+    dict(
+        type="MultiScaleFlipAug",
+        img_scale=(800, 800),
+        flip=False,
+        transforms=[
+            dict(type="RResize"),
+            dict(type="Normalize", **img_norm_cfg),
+            dict(type="Pad", size_divisor=32),
+            dict(type="DefaultFormatBundle"),
+            dict(type="Collect", keys=["img"]),
+        ],
+    ),
+]
+
+# Oversample rare classes following LVIS repeat-factor strategy:
 # r(c) = max(1, sqrt(t / f(c))) where f(c) is the fraction of images containing class c.
-# With RSAR, aircraft/car/tank/harbor are rare by image-frequency, so this helps them show up.
 data = dict(
+    samples_per_gpu=2,
+    workers_per_gpu=2,
     train=dict(
         type="ClassBalancedDataset",
         oversample_thr=0.1,
@@ -17,6 +55,21 @@ data = dict(
             pipeline=train_pipeline,
             version=angle_version,
         ),
-    )
+    ),
+    val=dict(
+        type=dataset_type,
+        ann_file=data_root + "val/annfiles",
+        img_prefix=data_root + "val/images",
+        cache_dir=data_root + "outputs/cache/rsar_dota",
+        pipeline=test_pipeline,
+        version=angle_version,
+    ),
+    test=dict(
+        type=dataset_type,
+        ann_file=data_root + "test/annfiles",
+        img_prefix=data_root + "test/images",
+        cache_dir=data_root + "outputs/cache/rsar_dota",
+        pipeline=test_pipeline,
+        version=angle_version,
+    ),
 )
-
--- configs/lora_target_ablation/retinanet_dinov3_timm_convnext_small_fpn_rsar_le90_lora_r16_stage23_10kiter_cb0p1.py
+++ configs/lora_target_ablation/retinanet_dinov3_timm_convnext_small_fpn_rsar_le90_lora_r16_stage23_10kiter_cb0p1.py
@@ -1,18 +1,10 @@
-_base_ = ["./retinanet_dinov3_timm_convnext_small_fpn_rsar_le90_lora_r16_stage23_10kiter.py"]
+_base_ = [
+    "./retinanet_dinov3_timm_convnext_small_fpn_rsar_le90_lora_r16_stage23_10kiter.py",
+    "../_base_/datasets/rsar_dota_le90_cb0p1.py",
+]
 
-# Use class-balanced oversampling for rare classes on RSAR 6-class.
+# Keep the same batch config as default RSAR iter-based runs.
 data = dict(
-    train=dict(
-        type="ClassBalancedDataset",
-        oversample_thr=0.1,
-        dataset=dict(
-            type=dataset_type,
-            ann_file=data_root + "train/annfiles",
-            img_prefix=data_root + "train/images",
-            cache_dir=data_root + "outputs/cache/rsar_dota",
-            pipeline=train_pipeline,
-            version=angle_version,
-        ),
-    )
+    samples_per_gpu=1,
+    workers_per_gpu=2,
 )
-
