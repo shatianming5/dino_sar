f99e4910bf182ab4446042a263880ee55d008a0c
shatianming5 <shatianming5@users.noreply.github.com>
2025-12-25 01:34:10 -0500

    cb: make lora cb0p1 configs self-contained

--- configs/dinov3_lora/retinanet_dinov3_timm_convnext_base_fpn_rsar_le90_lora_r16_10kiter_cb0p1.py
+++ configs/dinov3_lora/retinanet_dinov3_timm_convnext_base_fpn_rsar_le90_lora_r16_10kiter_cb0p1.py
@@ -1,10 +1,75 @@
 _base_ = [
     "./retinanet_dinov3_timm_convnext_base_fpn_rsar_le90_lora_r16_10kiter.py",
-    "../_base_/datasets/rsar_dota_le90_cb0p1.py",
 ]
 
-# Keep the same batch config as default RSAR iter-based runs.
+dataset_type = "RSARDOTADataset"
+data_root = "./"
+
+angle_version = "le90"
+
+img_norm_cfg = dict(mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True)
+
+train_pipeline = [
+    dict(type="LoadImageFromFile"),
+    dict(type="LoadAnnotations", with_bbox=True),
+    dict(type="RResize", img_scale=(800, 800)),
+    dict(
+        type="RRandomFlip",
+        flip_ratio=[0.25, 0.25, 0.25],
+        direction=["horizontal", "vertical", "diagonal"],
+        version=angle_version,
+    ),
+    dict(type="Normalize", **img_norm_cfg),
+    dict(type="Pad", size_divisor=32),
+    dict(type="DefaultFormatBundle"),
+    dict(type="Collect", keys=["img", "gt_bboxes", "gt_labels"]),
+]
+
+test_pipeline = [
+    dict(type="LoadImageFromFile"),
+    dict(
+        type="MultiScaleFlipAug",
+        img_scale=(800, 800),
+        flip=False,
+        transforms=[
+            dict(type="RResize"),
+            dict(type="Normalize", **img_norm_cfg),
+            dict(type="Pad", size_divisor=32),
+            dict(type="DefaultFormatBundle"),
+            dict(type="Collect", keys=["img"]),
+        ],
+    ),
+]
+
 data = dict(
     samples_per_gpu=1,
     workers_per_gpu=2,
+    train=dict(
+        type="ClassBalancedDataset",
+        oversample_thr=0.1,
+        dataset=dict(
+            type=dataset_type,
+            ann_file=data_root + "train/annfiles",
+            img_prefix=data_root + "train/images",
+            cache_dir=data_root + "outputs/cache/rsar_dota",
+            pipeline=train_pipeline,
+            version=angle_version,
+        ),
+    ),
+    val=dict(
+        type=dataset_type,
+        ann_file=data_root + "val/annfiles",
+        img_prefix=data_root + "val/images",
+        cache_dir=data_root + "outputs/cache/rsar_dota",
+        pipeline=test_pipeline,
+        version=angle_version,
+    ),
+    test=dict(
+        type=dataset_type,
+        ann_file=data_root + "test/annfiles",
+        img_prefix=data_root + "test/images",
+        cache_dir=data_root + "outputs/cache/rsar_dota",
+        pipeline=test_pipeline,
+        version=angle_version,
+    ),
 )
